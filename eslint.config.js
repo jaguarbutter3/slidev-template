a7f21e1273213875ee38055a78321f137a369644
jaguarbutter3 <jaguarbutter3@gmail.com>
2026-03-29 23:20:26 +0900

    feat: initial slidev template setup

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+import markdown from '@eslint/markdown';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', '.slidev/**'],
+  },
+  // JavaScript & Vue の設定
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    rules: {
+      ...js.configs.recommended.rules,
+      'vue/multi-word-component-names': 'off',
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    files: ['**/*.md'],
+    plugins: {
+      markdown,
+    },
+    language: 'markdown/gfm',
+  },
+];
