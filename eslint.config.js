b88d5983dbcd838207f0fe5b1448f15d44baf6f5
jaguarbutter3 <jaguarbutter3@gmail.com>
2026-03-30 01:20:37 +0900

    chore: ipdate formatter

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import pluginVue from 'eslint-plugin-vue';
-import markdown from '@eslint/markdown';
+import js from '@eslint/js'
+import globals from 'globals'
+import pluginVue from 'eslint-plugin-vue'
+import markdown from '@eslint/markdown'
 
 export default [
   {
-    ignores: ['node_modules/**', 'dist/**', '.slidev/**'],
+    ignores: ['node_modules/**', 'dist/**', '.slidev/**']
   },
   // JavaScript & Vue の設定
   ...pluginVue.configs['flat/recommended'],
@@ -13,23 +13,27 @@ export default [
     files: ['**/*.{js,mjs,cjs,vue}'],
     rules: {
       ...js.configs.recommended.rules,
-      'vue/multi-word-component-names': 'off',
+      'vue/multi-word-component-names': 'off'
     },
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: {
         ...globals.browser,
-        ...globals.node,
-      },
-    },
+        ...globals.node
+      }
+    }
   },
 
   {
     files: ['**/*.md'],
     plugins: {
-      markdown,
+      markdown
     },
-    language: 'markdown/gfm',
+    language: 'markdown/gfm'
   },
-];
+
+  {
+    extends: ['some-config-standard', 'prettier']
+  }
+]
